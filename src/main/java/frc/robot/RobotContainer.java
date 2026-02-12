669c1fbea3d18d2539c8752b017527e3e6668ecc
Meemkoo if this user is still here bad things have happened <guy99992@gmail.com>
2026-02-12 16:19:30 -0600

    Broken from brendans thing. AUTON BAD

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -29,12 +29,12 @@ import frc.robot.subsystems.Drivetrain;
 
 public class RobotContainer {
     private double MaxSpeed = 1.0 * TunerConstantsV2.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-    // private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
-    private double MaxAngularRate = RotationsPerSecond.of(30*0.75).in(RadiansPerSecond);
+    private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    // private double MaxAngularRate = RotationsPerSecond.of(30*0.75).in(RadiansPerSecond);
 
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.09/2).withRotationalDeadband(MaxAngularRate * 0.1/2) // Add a 10% deadband
+            .withDeadband(MaxSpeed * 0.09).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
@@ -44,8 +44,6 @@ public class RobotContainer {
     private final CommandXboxController joystick = new CommandXboxController(0);
 
     public final Drivetrain drivetrain = TunerConstantsV2.createDrivetrain();
-
-    private double bestiespeed = 0;
 
     private Vision vision;
 
@@ -70,8 +68,8 @@ public class RobotContainer {
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed * joystick.getRightTriggerAxis()) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed * joystick.getRightTriggerAxis()) // Drive left with negative X (left)
+                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed)// * joystick.getRightTriggerAxis()) // Drive forward with negative Y (forward)
+                    .withVelocityY(-joystick.getLeftX() * MaxSpeed)// * joystick.getRightTriggerAxis()) // Drive left with negative X (left)
                     .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
             )
         );
